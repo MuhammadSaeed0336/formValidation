d5b7db16742235e6c558cc90d2b1495314975f6f
Muhammad Saeed <MuhammadSaeed0336>
2023-04-05 14:49:40 +0500

    form validation

---
+++ src/components/FormInput.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./FormInput.css";
+
+function FormInput(props) {
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleFocus = (e) => {
+    e.preventDefault();
+    setFocused(true);
+  };
+  return (
+    <div className="formInput">
+      <label>{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
+    </div>
+  );
+}
+
+export default FormInput;
